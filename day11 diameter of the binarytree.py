c4a05a2d4a216f8382eb8040a091dfb359ad2ecf
nagasai17 <62430771+nagasai17@users.noreply.github.com>
2020-04-12 16:52:57 +0530

    Create day11 diameter of the binarytree.py

---
+++ day11 diameter of the binarytree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        self.ans = 1
+        def depth(node):
+            if not node: return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
+
+        depth(root)
+        return self.ans - 1
